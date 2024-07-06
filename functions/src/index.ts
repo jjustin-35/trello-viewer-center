012fb7eec7ec1958a36f675959b1f4b3ad405bf0
jjustin-chen <justin.chen@kdanmobile.com>
2024-07-06 15:51:55 +0800

    update: add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,36 +1,36 @@
-import * as functions from "firebase-functions";
+import { onRequest } from "firebase-functions/v2/https";
 import admin from "firebase-admin";
 
 admin.initializeApp();
 
 const db = admin.firestore();
 
-export const getTrelloCredentials = functions.https.onRequest(
-  async (_, res) => {
-    try {
-      const doc = await db.collection("credentials").doc("trello").get();
-      if (!doc.exists) {
-        res.status(404).send("No credentials found");
-        return;
-      }
-      res.status(200).json(doc.data());
-    } catch (error) {
-      res.status(500).send(error.toString());
-    }
-  }
-);
+const corsPolicy = {
+  cors: ["http://localhost:3000"],
+};
 
-export const setTrelloCredentials = functions.https.onRequest(
-  async (req, res) => {
-    const { api_key, api_token } = req.body;
-    try {
-      await db
-        .collection("credentials")
-        .doc("trello")
-        .set({ api_key, api_token });
-      res.status(200).send("Credentials saved successfully");
-    } catch (error) {
-      res.status(500).send(error.toString());
+export const getTrelloCredentials = onRequest(corsPolicy, async (_, res) => {
+  try {
+    const doc = await db.collection("credentials").doc("trello").get();
+    if (!doc.exists) {
+      res.status(404).send("No credentials found");
+      return;
     }
+    res.status(200).json(doc.data());
+  } catch (error) {
+    res.status(500).send(error.toString());
   }
-);
+});
+
+export const setTrelloCredentials = onRequest(corsPolicy, async (req, res) => {
+  const { api_key, api_token } = req.body;
+  try {
+    await db
+      .collection("credentials")
+      .doc("trello")
+      .set({ api_key, api_token });
+    res.status(200).send("Credentials saved successfully");
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+});
