8ecf4f4b6db9d19f40f70e0cbc8814e59401817f
jjustin-chen <justin.chen@kdanmobile.com>
2024-07-06 22:16:27 +0800

    fix: firebase deploy error

--- functions/src/config/index.ts
+++ functions/src/config/index.ts
@@ -1,7 +1,7 @@
-enum config {
-  LOCALHOST = "http://localhost:3000",
-  TRELLO = "https://api.trello.com/1",
-  BOARD_ID = "63880117d7188d0583a9c9c4",
-}
+const config = {
+  LOCALHOST: "http://localhost:3000",
+  TRELLO: "https://api.trello.com/1",
+  BOARD_ID: "63880117d7188d0583a9c9c4",
+};
 
 export default config;
